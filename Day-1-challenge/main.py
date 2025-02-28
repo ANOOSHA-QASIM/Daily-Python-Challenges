7096ab204e19ac4474e903f1242736abdbc5d42f
anusha-qasim <anushatech333@gmail.com>
2025-02-28 20:36:45 +0500

    Add files via upload

---
+++ Day-1-challenge/main.py
@@ -0,0 +1,20 @@
+
+print("\n" + "=" * 50)
+title = "Day 1 Challenge"
+
+print(f"{title:^50}")
+name = "Anusha Qasim"
+language = "Python"
+print(f'"Hello, {language} Learners! 🚀 Today is the first step in our coding journey."')
+print(f'"My name is {name}, and I am excited to start learning {language}!"')
+
+
+year = "2025"
+goal = "Become a skilled Python Developer"
+print(f'"In {year} my goal is to {goal}!"')
+
+quote = "Success is the sum of small efforts, repeated day in and day out."
+print(f'"{quote}"')
+
+print("=" * 50)
+
